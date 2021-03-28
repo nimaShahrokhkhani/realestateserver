d7664ea75926b7187382ae2944fd86e79eefcb71
mohammad <farahani.mhd@gmail.com>
2021-03-28 18:15:49 +0430

    changes in db.js

--- helper/db.js
+++ helper/db.js
@@ -1,6 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017";
 var dbo = undefined;
+//collections
 const COLLECTIONS = {
     USERS: 'users',
     FILES: 'files',
