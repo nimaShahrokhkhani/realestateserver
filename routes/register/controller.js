8ff5535444d0af2a47ab5bae678aa72124c4293b
nima shahrokhkhani <nima.shahrokhkhani@gmail.com>
2021-05-26 03:12:32 +0430

    revision

--- routes/register/controller.js
+++ routes/register/controller.js
@@ -4,15 +4,53 @@ var db = require('../../helper/db');
 
 
 router.post('/', function (request, response, next) {
-    let filterData = {
+    let dataObject = {
         username: request.body.username,
+        name: request.body.name,
+        lastName: request.body.lastName,
+        userRole: request.body.userRole,
         password: request.body.password,
-        email: request.body.email,
+        shenaSName: request.body.shenaSName,
+        nationalId: request.body.nationalId,
+        birthDate: request.body.birthDate,
+        birthPlace: request.body.birthPlace,
+        address: request.body.address,
+        tel: request.body.tel,
+        recruitedDate: request.body.recruitedDate,
+        profession: request.body.profession,
+        minimumSalary: request.body.minimumSalary,
+        comment: request.body.comment,
+        priceFrom: request.body.priceFrom,
+        priceTo: request.body.priceTo,
+        priceFromMeter: request.body.priceFromMeter,
+        priceToMeter: request.body.priceToMeter,
+        insertFile: request.body.insertFile,
+        deleteFile: request.body.deleteFile,
+        editFile: request.body.editFile,
+        userType: request.body.userType,
+
     };
+
+    var filterData = {
+        username: request.body.username
+    };
+
     db.find(db.COLLECTIONS.USERS, filterData).then((users) => {
-        response.status(200).json(users);
+        if (_.isEmpty(users)) {
+            db.insert(db.COLLECTIONS.USERS, dataObject).then((res) => {
+                response.status(200).json(res);
+            }).catch((error) => {
+                response.status(409).send("User did not added");
+            });
+        } else {
+            response.status(409).send("User did not added");
+        }
     }).catch(() => {
-        response.status(409).send("Username not found");
+        db.insert(db.COLLECTIONS.USERS, dataObject).then((res) => {
+            response.status(200).json(res);
+        }).catch(() => {
+            response.status(409).send("User did not added");
+        });
     });
 });
 
